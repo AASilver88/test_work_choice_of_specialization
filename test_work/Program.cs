d824ab4fc3a1b7c562cbcf58775d887ecb2bb10c
AASilver88 <78360998+AASilver88@users.noreply.github.com>
2022-10-05 10:47:49 +0300

    Write program that, from an existing array of strings, forms an array of strings whose length is less than or equal to 3 characters.

--- test_work/Program.cs
+++ test_work/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Программа, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+Console.Clear();
+
+string[] array1 = new string[8] {"sdf", "2sbsb3", "D", "GASV", "AA", "D", "GASDVSV", "zA"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
